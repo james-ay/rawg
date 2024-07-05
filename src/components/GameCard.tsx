e73a3896dc4091712395e8e2082d44d950643355
James <jamesadrianymasa@gmail.com>
2024-07-05 14:05:09 +0800

    refactor of card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,24 +3,27 @@ import { Games } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../service/image-url";
+import GameCardContainer from "./GameCardContainer";
 export interface Props {
   game: Games;
 }
 const GameCard = ({ game }: Props) => {
   return (
     <>
-      <Card borderRadius={10} overflow="hidden" width="300px">
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-          <Heading fontSize="2x1">{game.name}</Heading>
-          <HStack justifyContent="space-between">
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-        </CardBody>
-      </Card>
+      <GameCardContainer>
+        <Card>
+          <Image src={getCroppedImageUrl(game.background_image)} />
+          <CardBody>
+            <Heading fontSize="2x1">{game.name}</Heading>
+            <HStack justifyContent="space-between">
+              <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+              />
+              <CriticScore score={game.metacritic} />
+            </HStack>
+          </CardBody>
+        </Card>
+      </GameCardContainer>
     </>
   );
 };
