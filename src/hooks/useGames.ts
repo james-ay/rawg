8684de838216e37630f5a5e8699bac38c071f5e8
James <jamesadrianymasa@gmail.com>
2024-07-05 15:05:47 +0800

    display genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,3 @@
-import { useState, useEffect } from "react";
-import apiClient from "../service/api-client";
-import { CanceledError } from "axios";
 import useData from "./useData";
 
 export interface Platform {
