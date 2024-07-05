43a938ae5c9b8fbbfe8253ea5dc4b8011d8fdf16
James <jamesadrianymasa@gmail.com>
2024-07-05 10:17:54 +0800

    chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;
